8a16c7eceef3b09ea2d7066e57076eb37dab97fd
Rastko Jokic <rastkojokic@gmail.com>
2016-10-29 19:10:27 +0000

    Empty issue module

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 var four0four = require('./utils/404');
 
 module.exports = function(app) {
+  app.use('/api/v1/issues', require('./api/v1/issues'));
   app.use('/', four0four.notFound);
 };
 
