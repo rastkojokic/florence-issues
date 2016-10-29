a714b8c3f285ab5471020217fe28440b2c908e6a
Rastko Jokic <rastkojokic@gmail.com>
2016-10-29 10:00:01 +0000

    Add base express server

---
+++ src/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var port = process.env.PORT || 8001;
+
+var environment = process.env.NODE_ENV;
+
+switch (environment) {
+  case 'test':
+    break
+  default:
+    break;
+}
+
+server.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+  console.log('env = ' + app.get('env') +
+    '\n__dirname = ' + __dirname +
+    '\nprocess.cwd = ' + process.cwd());
+});
+
