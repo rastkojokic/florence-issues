2336f2e3e242c9fcd560934510df230611a9d364
Rastko Jokic <rastkojokic@gmail.com>
2016-11-03 15:32:01 +0000

    Add issue verification for download and upload

--- src/api/v1/files/middlewares/index.js
+++ src/api/v1/files/middlewares/index.js
@@ -1,8 +1,15 @@
 var compose = require('composable-middleware');
 var handleUploads = require('./handleUploads');
+var verifyIssueParams = require('../../comments/middlewares/verifyParams');
 
 exports.upload = function() {
   return compose()
+    .use(verifyIssueParams)
     .use(handleUploads);
+};
+
+exports.download = function() {
+  return compose()
+    .use(verifyIssueParams);
 };
 
